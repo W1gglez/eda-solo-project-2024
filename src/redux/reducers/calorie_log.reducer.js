a2f28363549cb234243653dfff3bf5cae2ff8627
Logan Weigel <snr.wiggels@gmail.com>
2024-06-17 13:43:37 -0500

    Create calorie log reducer/saga

---
+++ src/redux/reducers/calorie_log.reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+const calorieLog = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_CALORIE_LOG':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  calorieLog,
+});
---
+++ src/redux/sagas/calorie_log.saga.js
@@ -0,0 +1,61 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchCalorieLog(action) {
+  try {
+    const result = yield axios.get('/api/calorie_tracker');
+    yield put({ type: 'SET_CALORIE_LOG', payload: result.data });
+  } catch (err) {
+    console.log('Calorie Log GET request failed', err);
+  }
+}
+
+function* createNewLog(action) {
+  try {
+    yield axios.post('/api/calorie_tracker/add-log');
+    yield put({ type: 'FETCH_CALORIE_LOG' });
+  } catch (err) {
+    console.log('Create new log POST request failed', err);
+  }
+}
+
+function* addLogEntry(action) {
+  try {
+    yield axios.post('/api/calorie_tracker/add-log', action.payload);
+    yield put({ type: 'FETCH_CALORIE_LOG' });
+  } catch (err) {
+    console.log('Add log entry POST request failed', err);
+  }
+}
+
+function* updateEntry(action) {
+  try {
+    yield axios.put(
+      `/api/calorie_tracker/update-entry/${action.payload.id}`,
+
+      action.payload
+    );
+    yield put({ type: 'FETCH_CALORIE_LOG' });
+  } catch (err) {
+    console.log('Update log PUT request failed', err);
+  }
+}
+
+function* removeEntry(action) {
+  try {
+    yield axios.delete(`/api/calorie_tracker/remove-entry/${action.payload}`);
+    yield put({ type: 'FETCH_CALORIE_LOG' });
+  } catch (err) {
+    console.log('Remove entry DELETE failed', err);
+  }
+}
+
+function* calorieLogSaga() {
+  yield takeLatest('FETCH_CALORIE_LOG', fetchCalorieLog);
+  yield takeLatest('CREATE_LOG', createNewLog);
+  yield takeLatest('ADD_ENTRY', addLogEntry);
+  yield takeLatest('UPDATE_ENTRY', updateEntry);
+  yield takeLatest('DELETE_ENTRY', removeEntry);
+}
+
+export default calorieLogSaga;
